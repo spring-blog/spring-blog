c16aacca5e7078a21ea66ac2cc38101a3795fb23
AMinSC <cdtjdcjf@gmail.com>
2025-03-27 22:14:46 +0900

    Add post entity class

---
+++ src/main/java/com/springblog/springblog/domain/Post.java
@@ -0,0 +1,31 @@
+package com.springblog.springblog.domain;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int postId;
+
+    // User Entity Class
+    private String author;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "createdAt", nullable = false)
+    private Date createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private Date updatedAt;
+
+    public Post() {}
+
+}
