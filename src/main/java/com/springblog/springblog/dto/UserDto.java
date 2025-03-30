dde3097b4a7d11b2261ff496d7b7076112455848
nohheeyeon <heeyeon1213@gmail.com>
2025-03-30 18:05:43 +0900

    feat : UserDto 클래스 추가
    - 회원가입을 위한 데이터 전송 객체(UserDto) 생성
    - username, password, email 필드를 포함

---
+++ src/main/java/com/springblog/springblog/dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.springblog.springblog.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserDto {
+    private String username;
+    private String password;
+    private String email;
+}
+
