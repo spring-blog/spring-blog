2cc5b7d90e5c7a31840e08f1bd096d28d21a30ad
nohheeyeon <heeyeon1213@gmail.com>
2025-03-30 18:02:27 +0900

    feat : User 엔티티 클래스 추가
    - User 엔티티 클래스를 추가하여 회원 정보를 관리
    - id, username, password, email 필드를 포함한 기보 생성자 및 DTO -> Entity 변환을 위한 생성자 추가

---
+++ src/main/java/com/springblog/springblog/domain/User.java
@@ -0,0 +1,27 @@
+package com.springblog.springblog.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {}
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+}
