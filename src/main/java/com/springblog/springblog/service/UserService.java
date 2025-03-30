688d19f21720af22101abe25d51f9c714a5df923
nohheeyeon <heeyeon1213@gmail.com>
2025-03-30 18:08:12 +0900

    feat : UserService 추상 클래스 추가
    - 회원가입을 위한 signUp 메서드를 추상 메서드로 정의
    - UserDto를 매개변수로 받아 회원가입 기능을 처리할 예정

---
+++ src/main/java/com/springblog/springblog/service/UserService.java
@@ -0,0 +1,8 @@
+package com.springblog.springblog.service;
+
+import com.springblog.springblog.dto.UserDto;
+
+public abstract class UserService {
+
+    public abstract void signUp(UserDto userDto);
+}
